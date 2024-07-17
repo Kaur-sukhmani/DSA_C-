b09e5da8179f3e37bdac2cbe1755b6dae1e2dbb0
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2024-07-17 08:02:26 +0530

    Element Appearing More Than 25% In Sorted Array

---
+++ Hashmap/leetcode/map001.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/description/
+/*
+Element Appearing More Than 25% In Sorted Array
+
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+Example 2:
+
+Input: arr = [1,1]
+Output: 1
+ 
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 105
+*/
+
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+        // creation
+        unordered_map<int,int> m;
+        // inserting
+        for(auto i : arr){
+            m[i]++;
+        }
+        // iterating 
+        int maxi = 0;
+        int ans = 0;
+        for(auto i : m){
+            if(maxi < i.second){
+                maxi = i.second;    
+                ans = i.first;
+            }
+        }
+        return ans;
+    }
+};
