913d3734cfd2ee0e79614e3b1a79b2aeac17c3a6
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2024-07-19 06:03:52 +0530

    intro to graph

---
+++ Graph/intro.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    /*ADJACENCY LIST */
+    vector<int> adj[n+1];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+   /* ADJACENCY MATRIX*/
+    // int adj[n+1][m+1];//for 1 base indexing  
+    // for(int i=0;i<m;i++){//m is the number of lines
+    //     int u,v;
+    //     cin>>u>>v;
+    //     adj[u][v]=1;
+    //     adj[v][u]=1;
+    // }
+    return 0;
+}
