33df64c4bb3b87b3c15800dc0bc42d122ee4fe22
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2024-07-27 09:47:51 +0530

    Number of Distinct Islands

---
+++ Graph/gfg-leetcode/graph012.cpp
@@ -0,0 +1,124 @@
+/*
+Number of Distinct Islands
+https://www.geeksforgeeks.org/problems/number-of-distinct-islands/0
+
+Difficulty: MediumAccuracy: 62.29%Submissions: 75K+Points: 4
+Given a boolean 2D matrix grid of size n * m. You have to find the number of distinct islands where a group of connected 1s (horizontally or vertically) forms an island. Two islands are considered to be distinct if and only if one island is not equal to another (not rotated or reflected).
+
+Example 1:
+
+Input:
+grid[][] = {{1, 1, 0, 0, 0},
+            {1, 1, 0, 0, 0},
+            {0, 0, 0, 1, 1},
+            {0, 0, 0, 1, 1}}
+Output:
+1
+Explanation:
+grid[][] = {{1, 1, 0, 0, 0}, 
+            {1, 1, 0, 0, 0}, 
+            {0, 0, 0, 1, 1}, 
+            {0, 0, 0, 1, 1}}
+Same colored islands are equal.
+We have 2 equal islands, so we 
+have only 1 distinct island.
+
+Example 2:
+
+Input:
+grid[][] = {{1, 1, 0, 1, 1},
+            {1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1},
+            {1, 1, 0, 1, 1}}
+Output:
+3
+Explanation:
+grid[][] = {{1, 1, 0, 1, 1}, 
+            {1, 0, 0, 0, 0}, 
+            {0, 0, 0, 0, 1}, 
+            {1, 1, 0, 1, 1}}
+Same colored islands are equal.
+We have 4 islands, but 2 of them
+are equal, So we have 3 distinct islands.
+
+Your Task:
+
+You don't need to read or print anything. Your task is to complete the function countDistinctIslands() which takes the grid as an input parameter and returns the total number of distinct islands.
+
+Expected Time Complexity: O(n * m)
+Expected Space Complexity: O(n * m)
+*/
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+
+
+class Solution {
+  public:
+    void dfs(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& vis, vector<pair<int,int>>& vec, int row0, int col0) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vis[row][col] = 1;
+        vec.push_back({row - row0, col - col0});
+
+        int drow[] = {-1, 0, +1, 0};
+        int dcol[] = {0, -1, 0, +1};
+
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
+                dfs(nrow, ncol, grid, vis, vec, row0, col0);
+            }
+        }
+    }
+
+    int countDistinctIslands(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        set<vector<pair<int, int>>> st;
+        // TC->N*M*log(N*M)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && grid[i][j] == 1) {
+                    vector<pair<int, int>> vec;
+                    // TC->N*M*4
+                    dfs(i, j, grid, vis, vec, i, j);
+                    st.insert(vec);
+                }
+            }
+        }
+        // N*M*log(N*M)+N*M*4
+        return st.size();
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.countDistinctIslands(grid) << endl;
+    }
+}
+// } Driver Code Ends
