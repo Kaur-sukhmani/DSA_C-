7f24abaf4494ab4954777d3dc7df09e2293de037
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2024-07-31 16:16:22 +0530

    Topological sort DFS

---
+++ Graph/gfg-leetcode/graph017.cpp
@@ -0,0 +1,137 @@
+/*
+Topological sort
+https://www.geeksforgeeks.org/problems/topological-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=topological-sort
+
+Given an adjacency list for a Directed Acyclic Graph (DAG) where adj_list[i] contains a list of all vertices j such that there is a directed edge from vertex i to vertex j, with  V  vertices and E  edges, your task is to find any valid topological sorting of the graph.
+
+ 
+
+In a topological sort, for every directed edge u -> v,  u must come before v in the ordering.
+
+ 
+
+Example 1:
+
+Input:
+
+Output:
+1
+Explanation:
+The output 1 denotes that the order is
+valid. So, if you have, implemented
+your function correctly, then output
+would be 1 for all test cases.
+One possible Topological order for the
+graph is 3, 2, 1, 0.
+Example 2:
+
+Input:
+
+Output:
+1
+Explanation:
+The output 1 denotes that the order is
+valid. So, if you have, implemented
+your function correctly, then output
+would be 1 for all test cases.
+One possible Topological order for the
+graph is 5, 4, 2, 1, 3, 0.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function topoSort()  which takes the integer V denoting the number of vertices and adjacency list as input parameters and returns an array consisting of the vertices in Topological order. As there are multiple Topological orders possible, you may return any of them. If your returned topo sort is correct then the console output will be 1 else 0.
+
+Expected Time Complexity: O(V + E).
+Expected Auxiliary Space: O(V).
+
+
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+	void dfs(int node, stack<int>& s, vector<int>& vis, vector<int> adj[]) {
+        vis[node] = 1;
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                dfs(it, s, vis, adj);
+            }
+        }
+        s.push(node);
+    }
+
+    // Function to return list containing vertices in Topological order. 
+    vector<int> topoSort(int V, vector<int> adj[]) {
+        vector<int> vis(V, 0);
+        stack<int> s;
+        vector<int> topo;
+        
+        // Traversing all components
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) {
+                dfs(i, s, vis, adj);
+            }
+        }
+        
+        // Extracting elements from stack to form topological order
+        while(!s.empty()) {
+            topo.push_back(s.top());
+            s.pop();
+        }
+        
+        return topo;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
