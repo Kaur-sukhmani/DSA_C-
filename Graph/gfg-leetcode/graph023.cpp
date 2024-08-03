6a28bed2bb0290ce102d67c41f546854d1de5b62
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2024-08-03 13:04:47 +0530

    Alien Dictionary(TopoSort BFS)

---
+++ Graph/gfg-leetcode/graph023.cpp
@@ -0,0 +1,163 @@
+/*Alien Dictionary
+https://www.geeksforgeeks.org/problems/alien-dictionary/1
+
+
+Given a sorted dictionary of an alien language having N words and k starting alphabets of standard dictionary. Find the order of characters in the alien language.
+Note: Many orders may be possible for a particular test case, thus you may return any valid order and output will be 1 if the order of string returned by the function is correct else 0 denoting incorrect string returned.
+Given a sorted dictionary of an alien language having N words and k starting alphabets of standard dictionary. Find the order of characters in the alien language.
+Note: Many orders may be possible for a particular test case, thus you may return any valid order and output will be 1 if the order of string returned by the function is correct else 0 denoting incorrect string returned.
+ 
+
+Example 1:
+
+Input: 
+N = 5, K = 4
+dict = {"baa","abcd","abca","cab","cad"}
+Output:
+1
+Explanation:
+Here order of characters is 
+'b', 'd', 'a', 'c' Note that words are sorted 
+and in the given language "baa" comes before 
+"abcd", therefore 'b' is before 'a' in output.
+Similarly we can find other orders.
+Example 2:
+
+Input: 
+N = 3, K = 3
+dict = {"caa","aaa","aab"}
+Output:
+1
+Explanation:
+Here order of characters is
+'c', 'a', 'b' Note that words are sorted
+and in the given language "caa" comes before
+"aaa", therefore 'c' is before 'a' in output.
+Similarly we can find other orders.
+ 
+
+Your Task:
+You don't need to read or print anything. Your task is to complete the function findOrder() which takes  the string array dict[], its size N and the integer K as input parameter and returns a string denoting the order of characters in the alien language.
+
+
+Expected Time Complexity: O(N * |S| + K) , where |S| denotes maximum length.
+Expected Space Compelxity: O(K)
+*/
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    vector<int> topoSort(int V, vector<int> adj[])  
+    {
+        vector<int> ans;
+        vector<int> indegree(V, 0); // Corrected the initialization of indegree array
+        
+        // Calculate indegree for each node
+        for(int i = 0; i < V; i++) {
+            for(auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+        
+        // Initialize the queue with all nodes having indegree 0
+        queue<int> q;
+        for(int i = 0; i < V; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        
+        // Process nodes in topological order
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            // Reduce indegree for all adjacent nodes
+            for(auto it : adj[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        return ans;
+    }
+    string findOrder(string dict[], int N, int K) {
+        //code here
+        vector<int> adj[K];//to store the adjacency list 
+        for(int i=0;i<N-1;i++){
+            string s1=dict[i];
+            string s2=dict[i+1];
+            int len=min(s1.size(),s2.size());
+            for(int ptr=0;ptr<len;ptr++){
+                if(s1[ptr]!=s2[ptr]){
+                    adj[s1[ptr]-'a'].push_back(s2[ptr]-'a');
+                    break;
+                }
+            }
+        }
+        vector<int> topoSortedNodes = topoSort(K, adj);
+        string ans="";
+        for(auto it:topoSortedNodes){
+            ans=ans+char(it+'a');
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+string order;
+bool f(string a, string b) {
+    int p1 = 0;
+    int p2 = 0;
+    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
+        p1 = order.find(a[i]);
+        p2 = order.find(b[i]);
+        //	cout<<p1<<" "<<p2<<endl;
+    }
+
+    if (p1 == p2 and a.size() != b.size()) return a.size() < b.size();
+
+    return p1 < p2;
+}
+
+// Driver program to test above functions
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+        string dict[N];
+        for (int i = 0; i < N; i++) cin >> dict[i];
+        
+        Solution obj;
+        string ans = obj.findOrder(dict, N, K);
+        order = "";
+        for (int i = 0; i < ans.size(); i++) order += ans[i];
+
+        string temp[N];
+        std::copy(dict, dict + N, temp);
+        sort(temp, temp + N, f);
+
+        bool f = true;
+        for (int i = 0; i < N; i++)
+            if (dict[i] != temp[i]) f = false;
+
+        if(f)cout << 1;
+        else cout << 0;
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
