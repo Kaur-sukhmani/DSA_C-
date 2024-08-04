d2c61a59822f60ee492fbe4a1365ea2856f6e3b8
Kaur-sukhmani <109385758+Kaur-sukhmani@users.noreply.github.com>
2024-08-05 03:21:25 +0530

    Dijkstra Algorithm using pq

---
+++ Graph/gfg-leetcode/graph027.cpp
@@ -0,0 +1,100 @@
+/*Dijkstra Algorithm using pq
+// https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=implementing-dijkstra-set-1-adjacency-matrix
+
+Given a weighted, undirected and connected graph of V vertices and an adjacency list adj where adj[i] is a list of lists containing two integers where the first integer of each list j denotes there is edge between i and j , second integers corresponds to the weight of that  edge . You are given the source vertex S and You to Find the shortest distance of all the vertex's from the source vertex S. You have to return a list of integers denoting shortest distance between each node and Source vertex S.
+ 
+
+Note: The Graph doesn't contain any negative weight cycle.
+
+ 
+
+Example 1:
+
+Input:
+V = 2
+adj [] = {{{1, 9}}, {{0, 9}}}
+S = 0
+Output:
+0 9
+Explanation:
+
+The source vertex is 0. Hence, the shortest 
+distance of node 0 is 0 and the shortest 
+distance from node 1 is 9.
+ 
+
+Example 2:
+
+Input:
+V = 3, E = 3
+adj = {{{1, 1}, {2, 6}}, {{2, 3}, {0, 1}}, {{1, 3}, {0, 6}}}
+S = 2
+Output:
+4 3 0
+Explanation:
+
+For nodes 2 to 0, we can follow the path-
+2-1-0. This has a distance of 1+3 = 4,
+whereas the path 2-0 has a distance of 6. So,
+the Shortest path from 2 to 0 is 4.
+The shortest distance from 0 to 1 is 1 .
+ 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function dijkstra()  which takes the number of vertices V and an adjacency list adj as input parameters and Source vertex S returns a list of integers, where ith integer denotes the shortest distance of the ith node from the Source node. Here adj[i] contains a list of lists containing two integers where the first integer j denotes that there is an edge between i and j and the second integer w denotes that the weight between edge i and j is w.
+
+ 
+
+Expected Time Complexity: O(V2).
+Expected Auxiliary Space: O(V2).
+
+ 
+
+Constraints:
+1 ≤ V ≤ 1000
+0 ≤ adj[i][j] ≤ 1000
+1 ≤ adj.size() ≤ [ (V*(V - 1)) / 2 ]
+0 ≤ S < V
+*/
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S) {
+        vector<int> dist(V, 1e9);  // Initialize distances with a large value
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        
+        dist[S] = 0;  // Distance from source to itself is 0
+        pq.push({0, S});  // Push the source node with distance 0
+        
+        while (!pq.empty()) {
+            int d = pq.top().first;  // Current distance
+            int node = pq.top().second;  // Current node
+            pq.pop();
+            
+            // Iterate through adjacent nodes of the current node
+            for (auto it : adj[node]) {
+                int adjNode = it[0];  // Adjacent node
+                int edgeWeight = it[1];  // Edge weight
+                
+                // If a shorter path to adjNode is found
+                if (dist[adjNode] > d + edgeWeight) {
+                    dist[adjNode] = d + edgeWeight;
+                    pq.push({dist[adjNode], adjNode});
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// TC->O(ElogV)
+
+// } Driver Code Ends
